550fca17628544d3cf826c52df50447db44e4d4f
Raginaakhter <raginaakhter052@gmail.com>
2026-01-14 23:58:00 +0600

    added demo

--- app/(WithCommonLayout)/service/page.tsx
+++ app/(WithCommonLayout)/service/page.tsx
@@ -7,7 +7,7 @@ const page = () => {
 
             {/* <Link  href="/dashboard">go to  Dashboard</Link> */}
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat, autem maxime. Rem aliquam debitis modi sint provident adipisci doloribus? Corrupti neque recusandae reiciendis
-             excepturi officia eius mollitia hic eos deleniti?
+             excepturi officia 
             
         </div>
     );
