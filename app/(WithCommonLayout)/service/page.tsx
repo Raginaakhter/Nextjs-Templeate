f246e55ccdd5f0c0b42c5f864742b9f779c972dd
Raginaakhter <raginaakhter052@gmail.com>
2026-01-14 23:56:58 +0600

    added

--- app/(WithCommonLayout)/service/page.tsx
+++ app/(WithCommonLayout)/service/page.tsx
@@ -6,6 +6,8 @@ const page = () => {
             This is service page  <br />
 
             {/* <Link  href="/dashboard">go to  Dashboard</Link> */}
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat, autem maxime. Rem aliquam debitis modi sint provident adipisci doloribus? Corrupti neque recusandae reiciendis
+             excepturi officia eius mollitia hic eos deleniti?
             
         </div>
     );
