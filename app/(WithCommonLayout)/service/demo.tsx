b33825835b3b8ce5005c7e91e87f3c7fb7d7a77c
Raginaakhter <raginaakhter052@gmail.com>
2026-01-15 00:04:44 +0600

    updated demo

--- app/(WithCommonLayout)/service/demo.tsx
+++ app/(WithCommonLayout)/service/demo.tsx
@@ -4,8 +4,8 @@ const demo = () => {
     return (
         <div>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum,
-             assumenda. Quis placeat expedita minima blanditiis amet dolore, inventore necessitatibus earum cumque accusantium!
-             Delectus, aliquam dolor repellendus dolore laborum obcaecati consectetur.
+             assumenda inventore necessitatibus earum cumque accusantium!
+    
         </div>
     );
 };
