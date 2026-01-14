87e47fe8c6f8f4bc0d5918b2ae60c04f28974fde
Raginaakhter <raginaakhter052@gmail.com>
2026-01-15 00:01:14 +0600

    updated page

--- app/(WithCommonLayout)/service/page.tsx
+++ app/(WithCommonLayout)/service/page.tsx
@@ -6,7 +6,7 @@ const page = () => {
             This is service page  <br />
 
             {/* <Link  href="/dashboard">go to  Dashboard</Link> */}
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat, autem maxime. Rem aliquam debitis modi sint provident adipisci doloribus? Corrupti neque recusandae reiciendis
+            Lorem ipsum dolor sit,ffff Quaerat, autem maxime. Rem aliquam debitis modi sint provident adipisci doloribus? Corrupti neque recusandae reiciendis
              excepturi officia 
             
         </div>
